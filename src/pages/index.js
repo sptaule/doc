bb44af766f65d545a17176f519ad6881876535bf
Sptaule <lecas83@gmail.com>
2021-10-05 14:44:06 +0200

    Update infos on pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <img src="../../static/img/logo-full-white.svg" alt="Scuba" width='300'/>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
