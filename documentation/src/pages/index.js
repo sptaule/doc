e0ec1897eaa5894c0f876ed7c66a9181261c4648
Sptaule <lecas83@gmail.com>
2021-10-06 11:45:59 +0200

    Mise à jour des tags meta

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Documentation`}
+      description="Documentation pour Scuba, la solution de gestion pour les clubs de plongée">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
