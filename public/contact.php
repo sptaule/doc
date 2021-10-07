596f3e519a183536defc00173005ef781b4faa2f
Sptaule <lecas83@gmail.com>
2021-10-07 23:50:29 +0200

    Deleted random static pages

--- public/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use duncan3dc\Laravel\BladeInstance;
-
-require '../bootstrap.php';
-require '../vendor/autoload.php';
-
-$blade = new BladeInstance(BASE_PATH . "/views", BASE_PATH . "/cache/views");
-echo $blade->render("sections.static.contact");
