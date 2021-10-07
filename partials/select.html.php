2fe270c77113f2cd3de0697550b38823bee183d5
Sptaule <lecas83@gmail.com>
2021-10-07 23:49:58 +0200

    Update partials select style

--- partials/select.html.php
+++ partials/select.html.php
@@ -1,10 +1,4 @@
 <style>
-    html.theme-dark .nice-select ul.list {
-        background: #1a1c23;
-    }
-    html.theme-dark .nice-select ul.list li:hover, html.theme-dark .nice-select ul.list li.option.selected.focus, html.theme-dark .nice-select ul.list li.option.selected.focus:hover {
-        background: #383a42;
-    }
     .nice-select { height: 49px !important; line-height: 43px !important; font-size: 15px !important; margin-top: 3px !important; border: 2px solid #D1D5DB !important; }
 </style>
 
@@ -15,7 +9,7 @@
             <option value="<?= $session_option_value ?? $default_option_value ?? "" ?>"><?= $session_option_label ?? $default_option_label ?></option>
         <?php endif; ?>
         <?php foreach ($options as $option): ?>
-            <option class="<?= $option->colorclass ?? '' ?> text-white dark:bg-gray-700" value="<?= $option->id ?>" <?= $option->id == ($_SESSION['search'][$name] ?? get_input($name) ?? $model->{$name} ?? null) ? 'selected' : '' ?>>
+            <option class="<?= $option->colorclass ?? '' ?> text-white" value="<?= $option->id ?>" <?= $option->id == ($_SESSION['search'][$name] ?? get_input($name) ?? $model->{$name} ?? null) ? 'selected' : '' ?>>
                 <?= $option->{$option_key_label} ?>
             </option>
         <?php endforeach ?>
