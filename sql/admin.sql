45a5e610759602a9f961125590f38a00caee0e72
Sptaule <lecas83@gmail.com>
2021-10-09 01:23:21 +0200

    Rename table

--- sql/admin.sql
+++ sql/admin.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `admins`
+CREATE TABLE `admin`
 (
     `id`       int(11)                                 NOT NULL AUTO_INCREMENT,
     `name`     varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -8,4 +8,7 @@ CREATE TABLE `admins`
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci
+  COLLATE = utf8mb4_unicode_ci;
+
+INSERT INTO `admin` (`id`, `name`, `email`, `password`) VALUES
+    (1, 'Lucas', 'lecas83@gmail.com', '$2y$10$8GzOB8itA7ynY6rtWYCMOOI4agxg4r6hb704fc9JMKDTXO2fp0zV6');
