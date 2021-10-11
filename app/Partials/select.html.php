ea80f77d4e4084681d1b274932b3be2fb79ed466
Sptaule <lecas83@gmail.com>
2021-10-11 10:59:54 +0200

    Update select design

--- app/Partials/select.html.php
+++ app/Partials/select.html.php
@@ -1,9 +1,9 @@
 <style>
-    .nice-select { height: 46px !important; line-height: 41px !important; font-size: 16px !important; margin-top: 3px !important; border: 2px solid #D1D5DB !important; color: #393a3d; }
+    .nice-select { height: 49px !important; line-height: 44px !important; font-size: 16px !important; margin-top: 1px !important; border: 2px solid #D1D5DB !important; color: #393a3d; }
 </style>
 
 <div class="<?= $size ?? '' ?>">
-    <label class="block px-1 text-sm text-gray-600" for="<?= $name ?>"><?= $label ?></label>
+    <label class="block px-1 text-sm text-gray-600 mt-0.5" for="<?= $name ?>"><?= $label ?></label>
     <select name="<?= $name ?>" id="<?= $name ?>" class="<?= $customSelectClass ?? "" ?> block dark:bg-gray-700 dark:text-white w-max focus:outline-none">
         <?php if (isset($session_option_label) || isset($default_option_label)): ?>
             <option value="<?= $session_option_value ?? $default_option_value ?? "" ?>"><?= $session_option_label ?? $default_option_label ?></option>
