24fc36cf4cb35f7695fc4d8ae995d205c3fad262
Sptaule <lecas83@gmail.com>
2021-10-14 23:55:18 +0200

    Updated style for partials with Tailwind Forms plugin

--- app/Partials/input.html.php
+++ app/Partials/input.html.php
@@ -1,6 +1,6 @@
 <div class="<?= $size ?? '' ?>">
     <label class="px-1 text-sm text-gray-600" for="<?= $name ?>"><?= $label ?></label>
-    <input class="<?= $class ?? '' ?> appearance-none block w-full bg-gray-50 text-gray-700 border-2 border-gray-300 rounded py-2.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+    <input class="<?= $class ?? '' ?> appearance-none block w-full bg-gray-50 text-gray-700 border-2 border-gray-300 rounded py-2.5 px-4 leading-tight focus:outline-none focus:bg-white focus:border-white"
            type="<?= $type ?? 'text' ?>"
            name="<?= $name ?>"
            id="<?= $name ?>"
