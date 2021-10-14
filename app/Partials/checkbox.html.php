34d172ec0e3af957688fd86c5f400ba99a4f4e4c
Sptaule <lecas83@gmail.com>
2021-10-14 23:52:13 +0200

    Updated style for partials with Tailwind Forms plugin

--- app/Partials/checkbox.html.php
+++ app/Partials/checkbox.html.php
@@ -1,14 +1,15 @@
 <div class="flex items-center mt-3">
     <input
         type="checkbox"
-        class="form-checkbox rounded shadow border border-blue-300 bg-opacity-30 h-7 w-7 dark:text-gray-900 cursor-pointer"
+        class="form-checkbox rounded shadow border text-scuba-green focus:outline-none border-blue-300 bg-opacity-30 h-7 w-7 cursor-pointer"
         name="<?= $name ?>"
         id="<?= $id ?>"
         <?php if (isset($model->{$name}) && $model->{$name} == 1) { echo 'checked'; } ?>
         <?= get_input($name) === 'on' ? 'checked' : '' ?>
         <?= $state ?? '' ?>
     >
-    <label class="inline-flex items-center cursor-pointer" id="<?= $id ?>" for="<?= $name ?>">
-        <span class="ml-2 text-sm text-gray-600"><?= $label ?></span>
+    <label class="ml-2 flex flex-col items-start cursor-pointer" id="<?= $id ?>" for="<?= $name ?>">
+        <span class="text-sm text-gray-600 font-semibold"><?= $label ?></span>
+        <span class="text-gray-500 text-sm italic"><?= $subLabel ?? '' ?></span>
     </label>
 </div>
--- app/Partials/radio.html.php
+++ app/Partials/radio.html.php
@@ -1,33 +1,14 @@
-<div class="p-3">
-    <span class="text-lg font-thin"><i class="far fa-arrow-alt-circle-right mr-1.5"></i><?= $group_label ?></span>
-    <div class="mt-2 <?= $divClass ?? '' ?>">
+<div class="p-3 flex flex-col space-y-2">
+    <span class="text-lg font-light rounded-lg text-gray-700"><i class="far fa-dot-circle mr-1.5 text-scuba-green"></i><?= $groupLabel ?></span>
+    <?php if (isset($subLabel)): ?>
+        <span class="text-sm text-gray-500 italic"><?= $subLabel ?></span>
+    <?php endif; ?>
+    <?php foreach ($options as $k => $option): ?>
         <label class="inline-flex items-center cursor-pointer">
-            <input type="radio" class="form-radio h-6 w-6 shadow-lg cursor-pointer" name="<?= $name ?>" id="<?= $option_id_one ?? '' ?>" value="<?= $option_value_one ?? 1 ?>" <?php if (isset($model) && $model->{$name} == 1) { echo 'checked'; } ?>>
-            <span class="ml-2"><?= $option_label_one ?? 'Oui' ?></span>
+            <input type="radio" class="focus:outline-none text-scuba-green focus:bg-white focus:border-white bg-white h-6 w-6 cursor-pointer" value="<?= $k ?>" name="<?= $name ?>" <?= $k == $value ? 'checked' : '' ?>>
+            <span class="ml-2"><?= $option ?? 'Option' ?></span>
         </label>
-        <label class="inline-flex items-center cursor-pointer <?= $labelClass ?? 'ml-6' ?>">
-            <input type="radio" class="form-radio h-6 w-6 shadow-lg cursor-pointer" name="<?= $name ?>" id="<?= $option_id_two ?? '' ?>" value="<?= $option_value_two ?? 0 ?>" <?php if (isset($model) && $model->{$name} == 0) { echo 'checked'; } ?>>
-            <span class="ml-2"><?= $option_label_two ?? 'Non' ?></span>
-        </label>
-        <?php if (isset($option_value_three)): ?>
-            <label class="inline-flex items-center cursor-pointer <?= $labelClass ?? 'ml-6' ?>">
-                <input type="radio" class="form-radio h-6 w-6 shadow-lg cursor-pointer" name="<?= $name ?>" id="<?= $option_id_three ?? '' ?>" value="<?= $option_value_three ?? 0 ?>" <?php if (isset($model) && $model->{$name} == 0) { echo 'checked'; } ?>>
-                <span class="ml-2"><?= $option_label_three ?? 'Non' ?></span>
-            </label>
-        <?php endif; ?>
-        <?php if (isset($option_value_four)): ?>
-            <label class="inline-flex items-center cursor-pointer <?= $labelClass ?? 'ml-6' ?>">
-                <input type="radio" class="form-radio h-6 w-6 shadow-lg cursor-pointer" name="<?= $name ?>" id="<?= $option_id_four ?? '' ?>" value="<?= $option_value_four ?? 0 ?>" <?php if (isset($model) && $model->{$name} == 0) { echo 'checked'; } ?>>
-                <span class="ml-2"><?= $option_label_four ?? 'Non' ?></span>
-            </label>
-        <?php endif; ?>
-        <?php if (isset($option_value_five)): ?>
-            <label class="inline-flex items-center cursor-pointer <?= $labelClass ?? 'ml-6' ?>">
-                <input type="radio" class="form-radio h-6 w-6 shadow-lg cursor-pointer" name="<?= $name ?>" id="<?= $option_id_five ?? '' ?>" value="<?= $option_value_five ?? 0 ?>" <?php if (isset($model) && $model->{$name} == 0) { echo 'checked'; } ?>>
-                <span class="ml-2"><?= $option_label_five ?? 'Non' ?></span>
-            </label>
-        <?php endif; ?>
-    </div>
+    <?php endforeach; ?>
 </div>
 
 <style>
