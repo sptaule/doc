86421947d0c7836d2b7ec5692dd6a1cc7b048d83
Sptaule <lecas83@gmail.com>
2021-10-16 00:01:23 +0200

    Added color input partial

---
+++ app/Partials/color.html.php
@@ -0,0 +1,3 @@
+<label for="<?= $name ?? 'color' ?>" class="flex flex-col items-center justify-center rounded-lg">
+    <input id="<?= $name ?? 'color' ?>" name="<?= $name ?? 'color' ?>" type="color" value="<?= $value ?? '' ?>" class="w-10 h-10 p-0 border-none">
+</label>
