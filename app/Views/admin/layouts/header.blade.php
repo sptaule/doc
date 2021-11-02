d884ac8accc1a74173b76bff1f0ac8061fde37e9
Sptaule <lecas83@gmail.com>
2021-11-02 14:27:02 +0100

    Added dynamic club logo on admin dashboard and quick public access button

--- app/Views/admin/layouts/header.blade.php
+++ app/Views/admin/layouts/header.blade.php
@@ -5,9 +5,9 @@
         <div class="flex items-center justify-between p-2">
             <!-- Navbar left -->
             <div class="flex items-center space-x-3">
-                <img class="p-2 lg:hidden" src="{{ LOGO }}" width="45px">
+                <img class="p-2 lg:hidden" src="{{ LOGO }}" width="45px" alt="">
                 <!-- Toggle sidebar button -->
-                <button @click="toggleSidebarMenu()" class="p-2 rounded-md focus:outline-none focus:ring">
+                <button @click="toggleSidebarMenu()" class="p-2 rounded-md focus:outline-none focus:ring ring-gray-500">
                     <i class="fas fa-angle-double-right text-gray-500" :class="{'transform transition-transform -rotate-180': isSidebarOpen}"></i>
                 </button>
             </div>
@@ -16,9 +16,14 @@
             <div class="relative flex items-center space-x-3">
                 <div class="items-center hidden space-x-3 md:flex">
 
+                    <a target="_blank" href="{{ PUBLIC_HOME }}" class="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus:ring ring-gray-500 flex items-center justify-center text-sm space-x-1.5">
+                        <svg class="w-5 h-5" fill="none" stroke="rgb(107, 114, 128)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
+                        <span>Voir le site</span>
+                    </a>
+
                     <!-- Documentation Button -->
-                    <a id="demo01" href="#animatedModal" class="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus:ring flex items-center justify-center text-sm space-x-1.5">
-                        <i class="text-gray-500 fas fa-book"></i>
+                    <a id="demo01" href="#animatedModal" class="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus:ring ring-gray-500 flex items-center justify-center text-sm space-x-1.5">
+                        <svg class="w-5 h-5" fill="none" stroke="rgb(107, 114, 128)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                         <span>Aide</span>
                     </a>
                     {{ partial('documentation') }}
@@ -26,9 +31,9 @@
                     <div class="relative" x-data="{ isOpen: false }">
                         <button
                             @click="isOpen = !isOpen"
-                            class="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus:ring"
+                            class="p-2 bg-gray-100 rounded-full hover:bg-gray-200 focus:outline-none focus:ring ring-gray-500"
                         >
-                            <i class="w-6 h--6 text-gray-500 fas fa-sliders-h"></i>
+                            <svg class="w-5 h-5" fill="none" stroke="rgb(107, 114, 128)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                         </button>
 
                         <!-- Dropdown card -->
@@ -59,7 +64,7 @@
                 <!-- avatar button -->
                 <div class="relative" x-data="{ isOpen: false }">
                     <button @click="isOpen = !isOpen"
-                            class="p-1 bg-gray-200 rounded-full focus:outline-none focus:ring">
+                            class="p-1 bg-gray-200 rounded-full focus:outline-none focus:ring ring-gray-500">
                         <img
                             class="object-cover w-8 h-8 rounded-full"
                             src="https://avatars.githubusercontent.com/u/5262097?v=4"
@@ -71,12 +76,19 @@
                     <div
                             @click.away="isOpen = false"
                             x-show.transition.opacity="isOpen"
-                            class="absolute mt-3 transform -translate-x-full bg-white rounded-md shadow-lg min-w-max z-50"
+                            class="origin-top-left absolute -left-4 top-8 transform -translate-x-3/4 mt-3 bg-white rounded-md shadow-lg w-64 z-50"
                     >
                         <div class="flex flex-col p-4 space-y-1 font-medium border-b">
-                            <span class="font-extralight text-gray-800">Bonjour</span>
-                            <span class="text-gray-800">Lucas Chaplain</span>
-                            <span class="text-sm text-gray-400">contact@lucaschaplain.design</span>
+                            <span class="font-extralight text-gray-800">Connecté en tant que</span>
+                            <span class="text-gray-800 leading-5">{{ session()->get('user')->firstname . ' ' . session()->get('user')->lastname }}</span>
+                            <span class="text-sm text-gray-400 leading-3">{{ session()->get('user')->email }}</span>
+                        </div>
+                        <!-- Sidebar footer -->
+                        <div class="flex-shrink-0 p-2 border-t max-h-14">
+                            <a href="{{ ADMIN_LOGOUT }}" class="flex items-center justify-center w-full px-4 py-2 space-x-1 font-medium tracking-wider uppercase bg-gray-100 hover:bg-red-500 text-gray-600 hover:text-white border rounded-md focus:outline-none focus:ring">
+                                <span class="h-6 w-6 flex items-center justify-center"><i class="fas fa-sign-out-alt"></i></span>
+                                <span>Déconnexion</span>
+                            </a>
                         </div>
                     </div>
                 </div>
