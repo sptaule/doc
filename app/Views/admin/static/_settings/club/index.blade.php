5630668a5859c2afa82d9139614efda2c0d15490
Sptaule <lecas83@gmail.com>
2021-11-02 14:31:43 +0100

    Fix appearance (colors)

--- app/Views/admin/static/_settings/club/index.blade.php
+++ app/Views/admin/static/_settings/club/index.blade.php
@@ -4,23 +4,32 @@
 
     <form action="" method="post" class="grid grid-flow-row grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-x-12 w-full space-y-2">
         @php echo csrf_input() @endphp
-        <div class="bg-teal-50 shadow-md p-2">
+        <div class="col-span-full">
+            <p class="block w-full bg-blue-100 px-8 py-2 font-arima text-xl border rounded-md shadow">Le club</p>
+        </div>
+        <div class="bg-blue-50 shadow-md p-2">
             {{ partial('input', ['name' => 'club_name', 'label' => 'Nom du club', 'default_value' => $clubName]) }}
         </div>
-        <div class="bg-teal-50 shadow-md p-2">
+        <div class="bg-blue-50 shadow-md p-2">
             {{ partial('input', ['name' => 'club_description', 'label' => 'Slogan du club', 'default_value' => $clubDescription]) }}
         </div>
-        <div class="col-span-full"></div>
-        <div class="bg-teal-50 shadow-md p-2">
+
+        <div class="col-span-full">
+            <p class="block w-full bg-yellow-100 px-8 py-2 mt-4 font-arima text-xl border rounded-md shadow">L'administrateur</p>
+        </div>
+        <div class="bg-yellow-50 shadow-md p-2">
             {{ partial('input', ['name' => 'super_user_firstname', 'label' => 'Prénom du super administrateur', 'default_value' => $superUserFirstname]) }}
         </div>
-        <div class="bg-teal-50 shadow-md p-2">
+        <div class="bg-yellow-50 shadow-md p-2">
             {{ partial('input', ['name' => 'super_user_lastname', 'label' => 'Nom du super administrateur', 'default_value' => $superUserLastname]) }}
         </div>
-        <div class="bg-teal-50 shadow-md p-2">
+        <div class="bg-yellow-50 shadow-md p-2">
             {{ partial('input', ['name' => 'super_user_email', 'label' => 'Email du super administrateur', 'default_value' => $superUserEmail]) }}
         </div>
-        <div class="col-span-full"></div>
+
+        <div class="col-span-full">
+            <p class="block w-full bg-teal-100 px-8 py-2 mt-4 font-arima text-xl border rounded-md shadow">Autorisations et formats</p>
+        </div>
         <div class="bg-teal-50 shadow-md p-2 flex flex-col items-start justify-start">
             <p class="text-gray-600 italic text-sm">Si vous souhaitez fermer les inscriptions, cochez la case ci-dessous</p>
             {{ partial('checkbox', [
@@ -65,26 +74,28 @@
                 ]])
             }}
         </div>
-        <div class="col-span-full"></div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'facebook_url', 'default_value' => $facebookUrl, 'label' => '<i class="fab fa-facebook-square text-blue-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">Facebook</span>']) }}
+        <div class="col-span-full">
+            <p class="block w-full bg-purple-100 px-8 py-2 mt-4 font-arima text-xl border rounded-md shadow">Réseaux</p>
         </div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'instagram_url', 'default_value' => $instagramUrl, 'label' => '<i class="fab fa-instagram-square text-purple-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">Instagram</span>']) }}
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'facebook_url', 'placeholder' => 'Lien Facebook du club', 'default_value' => $facebookUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/733/733547.png"><span class="leading-3 inline-flex">Facebook</span></div>']) }}
         </div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'youtube_url', 'default_value' => $youtubeUrl, 'label' => '<i class="fab fa-youtube-square text-red-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">YouTube</span>']) }}
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'instagram_url', 'placeholder' => 'Lien Instagram du club', 'default_value' => $instagramUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/2111/2111463.png"><span class="leading-3 inline-flex">Instagram</span></div>']) }}
         </div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'whatsapp_url', 'default_value' => $whatsappUrl, 'label' => '<i class="fab fa-whatsapp-square text-green-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">WhatsApp</span>']) }}
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'youtube_url', 'placeholder' => 'Lien YouTube du club', 'default_value' => $youtubeUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/1384/1384060.png"><span class="leading-3 inline-flex">YouTube</span></div>']) }}
         </div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'twitter_url', 'default_value' => $twitterUrl, 'label' => '<i class="fab fa-twitter-square text-cyan-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">Twitter</span>']) }}
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'whatsapp_url', 'placeholder' => 'Lien WhatsApp du club', 'default_value' => $whatsappUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/134/134937.png"><span class="leading-3 inline-flex">WhatsApp</span></div>']) }}
         </div>
-        <div class="bg-indigo-50 shadow-md p-2">
-            {{ partial('input', ['name' => 'snapchat_url', 'default_value' => $snapchatUrl, 'label' => '<i class="fab fa-snapchat-square text-yellow-500 align-base mr-0.5 text-2xl"></i> <span class="leading-3 inline-flex">Snapchat</span>']) }}
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'twitter_url', 'placeholder' => 'Lien Twitter du club', 'default_value' => $twitterUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/733/733579.png"><span class="leading-3 inline-flex">Twitter</span></div>']) }}
         </div>
-        <button type="submit" class="btn submit">Valider les modifications</button>
+        <div class="bg-purple-50 shadow-md p-2">
+            {{ partial('input', ['name' => 'snapchat_url', 'placeholder' => 'Lien Snapchat du club', 'default_value' => $snapchatUrl, 'label' => '<div class="h-max w-full flex items-center justify-center space-x-2"><img class="inline" src="https://cdn-icons-png.flaticon.com/32/1216/1216734.png"><span class="leading-3 inline-flex">Snapchat</span></div>']) }}
+        </div>
+        <button type="submit" class="btn submit py-4 col-span-full">Valider les modifications</button>
     </form>
 
 @endsection
