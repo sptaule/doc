8a667f330954eaef637c35e451f79f97b3dc91f5
Sptaule <lecas83@gmail.com>
2021-11-02 14:30:39 +0100

    Page title with club name now

--- app/Views/admin/layouts/head.blade.php
+++ app/Views/admin/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Scuba / Administration</title>
+<title> Administration / {{ \App\Models\Club::getValue('club_name') }}</title>
 
 @include('common.favicons')
 @include('admin.layouts.header.css')
